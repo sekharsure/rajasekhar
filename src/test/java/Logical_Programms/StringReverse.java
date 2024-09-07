55cc3c58dcb6691f1e6a89dfacfe7d35a120b42f
sekharsure <sekharsure4@gmail.com>
2024-09-07 16:24:39 +0530

    hi

---
+++ src/test/java/Logical_Programms/StringReverse.java
@@ -0,0 +1,14 @@
+package Logical_Programms;
+
+public class StringReverse {
+	public static void main(String[] args) {
+		String s1 ="sekhar";
+		String s2="";
+		for(int i=0;i<s1.length();i++) {
+			s2 =s1.charAt(i)+s2;
+			
+		}
+		System.out.println(s2);
+
+	}
+}
